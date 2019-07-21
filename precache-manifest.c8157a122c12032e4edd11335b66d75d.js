225defa9f52523975da1acd707e2be7e1dc7efd3
megfan <megfa@yahoo.com>
2019-07-21 21:00:01 +0200

    Updates

---
+++ precache-manifest.c8157a122c12032e4edd11335b66d75d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a8ecc4bfa7da770651546905e27cb4",
+    "url": "/RecipeSearch_with_Hooks/index.html"
+  },
+  {
+    "revision": "8044e2c841be89ecd21e",
+    "url": "/RecipeSearch_with_Hooks/static/css/main.d5351b6a.chunk.css"
+  },
+  {
+    "revision": "123b10ccd0d391146b1d",
+    "url": "/RecipeSearch_with_Hooks/static/js/2.479fc620.chunk.js"
+  },
+  {
+    "revision": "8044e2c841be89ecd21e",
+    "url": "/RecipeSearch_with_Hooks/static/js/main.66dbb6c5.chunk.js"
+  },
+  {
+    "revision": "93ea89fc1b83f96e86e6",
+    "url": "/RecipeSearch_with_Hooks/static/js/runtime~main.b72ed0ca.js"
+  },
+  {
+    "revision": "2fce37d8055f726313e4ac025ef63a5e",
+    "url": "/RecipeSearch_with_Hooks/static/media/kobby-mendez-idTwDKt2j2o-unsplash.2fce37d8.jpg"
+  },
+  {
+    "revision": "fc410468cf2679b9184a03da5f93c7b7",
+    "url": "/RecipeSearch_with_Hooks/static/media/sam-moghadam-EW8-YSIDgtk-unsplash.fc410468.jpg"
+  }
+]);
